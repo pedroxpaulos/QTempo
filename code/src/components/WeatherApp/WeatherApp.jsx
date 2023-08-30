aded1556cd276f6879e4943779e7105d386c2651
pedroxpaulos <paulos.pedro@gmail.com>
2023-08-30 15:35:55 +0100

    API and Search bar working

--- code/src/components/WeatherApp/WeatherApp.jsx
+++ code/src/components/WeatherApp/WeatherApp.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
 import './WeatherApp.css';
 import search_icon from '../Assets/search.png';
 import clear_icon from '../Assets/clear.png';
@@ -10,24 +12,61 @@ import wind_icon from '../Assets/wind.png';
 import humidity_icon from '../Assets/humidity.png';
 
 function WeatherApp() {
+	const [city, setCity] = useState('Lisbon');
+	const apiKey = '5a09174979f701aecb6c103fd62018a8';
+	const [weather, setWeather] = useState('');
+	const ref = useRef();
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setCity(ref.current.value);
+	};
+
+	const ApiCall = () => {
+		const baseURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+		console.log(city);
+		axios
+			.get(`${baseURL}`)
+			.then((res) => {
+				const realTemp = res.data.main.temp - 273.15;
+				setWeather({
+					descp: res.data.weather[0].description,
+					temp: realTemp.toFixed(0),
+					city: res.data.name,
+					humidity: res.data.main.humidity,
+					press: res.data.main.pressure,
+				});
+			})
+			.catch((e) => console.log(e));
+	};
+	useEffect(() => {
+		ApiCall();
+	});
 	return (
 		<div className="container">
 			<div className="top-bar">
-				<input type="text" className="cityInput" placeholder="search" />
-				<div className="search-icon">
-					<img src={search_icon} alt="" />
-				</div>
+				<form onSubmit={handleSubmit} action="" className="form">
+					<input
+						type="text"
+						className="cityInput"
+						placeholder="search"
+						name="loc"
+						ref={ref}
+					/>
+					<button className="search-icon" type="submit">
+						<img src={search_icon} alt="" />
+					</button>
+				</form>
 			</div>
 			<div className="weather-image">
 				<img src={cloud_icon} alt="" />
 			</div>
-			<div className="weather-temp">24ºC</div>
-			<div className="weather-location">London</div>
+			<div className="weather-temp">{weather.temp}ºC</div>
+			<div className="weather-location">{weather.city}</div>
 			<div className="data-container">
 				<div className="element">
 					<img src={humidity_icon} alt="" className="icon" />
 					<div className="data">
-						<div className="humidity-percent">64%</div>
+						<div className="humidity-percent">{weather.humidity}</div>
 						<div className="text">Humidity</div>
 					</div>
 				</div>
